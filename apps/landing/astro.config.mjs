91a0e6b31dc31cf29a85fac33092f3d61d9908c8
Arash Karimzadeh <a.karimzadeh@gmail.com>
2026-02-09 23:21:55 +0400

    fix site build

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -16,7 +16,8 @@ export default defineConfig({
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src')
-      }
+      },
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.astro']
     }
   }
 });
